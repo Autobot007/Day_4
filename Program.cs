5bec901b276617f4724484749f0768cd6fd4c751
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-01 15:51:04 +0530

    [Sarfaraaz] And or condition added

--- Program.cs
+++ Program.cs
@@ -24,6 +24,19 @@
                 Console.WriteLine("Else executed");
             }
 
+            if (random.Next(1, 10) < 5 && random.Next(1, 10) < 3) // and condition
+            {
+                Console.WriteLine("if Statement Executed");
+            }
+            else if (random.Next(1, 10) == 5 || random.Next(1, 10) < 5)// or condition
+            {
+                Console.WriteLine("Else if executed");
+            }
+            else
+            {
+                Console.WriteLine("Else executed");
+            }
+
         }
     }
 }
