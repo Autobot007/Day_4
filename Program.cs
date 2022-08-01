4e5ad13dcfad0603d2280a3cb670f32090024b5f
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-01 15:30:13 +0530

    [Sarfaraaz]- Sequences Added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day4
+{
+    class Ifelse
+    {
+       public static void Main(string[] args)
+        {
+            // Sequences ------> simple executable command
+
+            Console.WriteLine("Sequences");
+
+        }
+    }
+}
