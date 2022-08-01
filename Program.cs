823d0c5f93626200df94722eb9c175ba9883b0b6
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-01 15:39:54 +0530

    [Sarfaraaz]- Selections added

--- Program.cs
+++ Program.cs
@@ -8,6 +8,22 @@
 
             Console.WriteLine("Sequences");
 
+            Random random = new Random();
+
+            //Selections
+            if (random.Next(1,10)<5)
+            {
+                Console.WriteLine("if Statement Executed");
+            }
+            else if(random.Next(1, 10) == 5)
+            {
+                Console.WriteLine("Else if executed");
+            }
+            else
+            {
+                Console.WriteLine("Else executed");
+            }
+
         }
     }
 }
